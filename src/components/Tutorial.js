73658bb1497cf17c99876aaf74de653ce6502bca
Casey Vu <vucas@oregonstate.edu>
2022-02-09 16:43:30 -0800

    Added local JSON file. Added functions to display mock data from JSON. Made each paragraph from JSON a paragraph element.

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -1,12 +1,53 @@
 import React from "react";
-
+// Fake summary data
+import summaryData from "../data/summary.json";
 /**
  * Renders the tutorial below the meme section.
  * @returns None
  */
 export default function Tutorial() {
+  /**
+   * Returns the first paragraph from the Wikipedia summary.
+   * @param {String} content : Value of summary taken from the JSON file.
+   * @returns
+   */
+  function firstParagraph(content) {
+    let result = content.slice(0, 506);
+    return result;
+  }
+
+  /**
+   * Returns the second paragraph from the Wikipedia summary.
+   * @param {String} content : Value of summary taken from the JSON file.
+   * @returns
+   */
+  function secondParagraph(content) {
+    let result = content.slice(506, 1056);
+    return result;
+  }
+
+  /**
+   * Returns the third paragraph from the Wikipedia summary.
+   * @param {String} content : Value of summary taken from the JSON file.
+   * @returns
+   */
+  function thirdParagraph(content) {
+    let result = content.slice(1056);
+    return result;
+  }
   return (
     <main>
+      <h2 className="title--tutorial">What is a meme?</h2>
+      {summaryData.map((summaryDetail, index) => {
+        return <p>{firstParagraph(summaryDetail.summary)}</p>;
+      })}
+      {summaryData.map((summaryDetail, index) => {
+        return <p>{secondParagraph(summaryDetail.summary)}</p>;
+      })}
+      {summaryData.map((summaryDetail, index) => {
+        return <p>{thirdParagraph(summaryDetail.summary)}</p>;
+      })}
+
       <h2 className="title--tutorial">How to make a meme</h2>
       <ol className="meme--tutorial" type="1">
         <li>
