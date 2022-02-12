07f1e38a0a06b5e147d7ea492f2a37530ef5fd3a
Casey Vu <vucas@oregonstate.edu>
2022-02-11 16:54:15 -0800

    Changed Tutorial component. Added download tutorial. Removed fake summary content.

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -1,71 +1,39 @@
 import React from "react";
-// Fake summary data
-import summaryData from "../data/summary.json";
+import Stack from "@mui/material/Stack";
+
 /**
  * Renders the tutorial below the meme section.
- * @returns None
  */
 export default function Tutorial() {
-  /**
-   * Returns the first paragraph from the Wikipedia summary.
-   * @param {String} content : Value of summary taken from the JSON file.
-   * @returns {String} result: Text of a paragrpah from JSON.
-   */
-  function firstParagraph(content) {
-    let result = content.slice(0, 506);
-    return result;
-  }
-
-  /**
-   * Returns the second paragraph from the Wikipedia summary.
-   * @param {String} content : Value of summary taken from the JSON file.
-   * @returns {String} result: Text of a paragrpah from JSON.
-   */
-  function secondParagraph(content) {
-    let result = content.slice(506, 1056);
-    return result;
-  }
-
-  /**
-   * Returns the third paragraph from the Wikipedia summary.
-   * @param {String} content : Value of summary taken from the JSON file.
-   * @returns {String} result: Text of a paragrpah from JSON.
-   */
-  function thirdParagraph(content) {
-    let result = content.slice(1056);
-    return result;
-  }
   return (
     <main>
-      <h2 className="title--tutorial">What is a meme?</h2>
-      {/* Add unique key to each JSON slice paragraph to avoid list warning */}
-      {summaryData.map((summaryDetail) => {
-        return <p key="1">{firstParagraph(summaryDetail.summary)}</p>;
-      })}
-      {summaryData.map((summaryDetail) => {
-        return <p key="2">{secondParagraph(summaryDetail.summary)}</p>;
-      })}
-      {summaryData.map((summaryDetail) => {
-        return <p key="3">{thirdParagraph(summaryDetail.summary)}</p>;
-      })}
-      <h2 className="title--tutorial">How to make a meme</h2>
-      <ol className="meme--tutorial">
-        <li>
-          <span className="bold">Choose an image.</span> You can click on
-          Generate new meme image to load a new image.
-        </li>
-        <li>
-          <span className="bold">Add customizations.</span> You can add text to
-          the bottom or top of the image.
-        </li>
-      </ol>
-      <h2 className="title--reset">How to make a reset text</h2>
-      <ol className="reset--tutorial">
-        <li>
-          <span className="bold">Reset text.</span> You can reset the text by
-          clicking the Reset button.
-        </li>
-      </ol>
+      <Stack spacing={2}>
+        <h2 className="title--tutorial">How to make a meme</h2>
+        <ol className="tutorial--meme">
+          <li>
+            <span className="bold">Load a random meme.</span> You can click on
+            Generate new meme image to load a new image.
+          </li>
+          <li>
+            <span className="bold">Add customizations.</span> You can add text
+            to the top or bottom of the image.
+          </li>
+        </ol>
+        <h2 className="title--reset">How to make a reset text</h2>
+        <ol className="tutorial--reset">
+          <li>
+            <span className="bold">Reset text.</span> You can reset the text by
+            clicking the Reset button.
+          </li>
+        </ol>
+        <h2 className="title--download">How to download a meme</h2>
+        <ol className="tutorial--download">
+          <li>
+            <span className="bold">Download a meme.</span> Once a meme is
+            loaded, click on the Download button to download the image.
+          </li>
+        </ol>
+      </Stack>
     </main>
   );
 }
