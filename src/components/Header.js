78d000dfffdc793219d1a755145ccd285f9cf43f
Casey Vu <vucas@oregonstate.edu>
2022-02-12 18:50:55 -0800

    Made nav image and header refresh the page. Added tooltip to img in Header component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,31 @@
 import React from "react";
 
+import Tooltip from "@mui/material/Tooltip";
+
 /**
  * Renders the header with the logo and title.
  * @returns
  */
 export default function Header() {
+  /**
+   * Refersh the page to reset the state.
+   */
+  function refreshPage() {
+    window.location.reload(false);
+  }
   return (
     <header className="header">
-      <img
-        src="../assets/pika-logo.png"
-        className="header--image"
-        alt="Surprised Pika Face"
-      />
-      <h2 className="header--title">Meme Generator</h2>
+      <Tooltip title="Click me to reset the page!" placement="bottom">
+        <img
+          onClick={refreshPage}
+          src="../assets/pika-logo.png"
+          className="header--image"
+          alt="Surprised Pika Face"
+        />
+      </Tooltip>
+      <h2 onClick={refreshPage} className="header--title">
+        Meme Generator
+      </h2>
     </header>
   );
 }
