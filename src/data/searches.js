bb561848d570bd098a98fc1d5db47e8159a86710
Casey Vu <vucas@oregonstate.edu>
2022-02-20 18:15:17 -0800

    Created microservice variant branch. Deleted Fake data. Added useEffect function to search using a random search parameter to teammate's API. Implemented teammate's service.

---
+++ src/data/searches.js
@@ -0,0 +1,31 @@
+export default {
+  data: {
+    articles: [
+      {
+        id: "1",
+        name: "Internet Meme",
+        search: "internet_meme",
+      },
+      {
+        id: "2",
+        name: "Meme",
+        search: "meme",
+      },
+      {
+        id: "3",
+        name: "Image Macro",
+        search: "image_macro",
+      },
+      {
+        id: "4",
+        name: "Internet Phenomena",
+        search: "list_of_Internet_phenomena",
+      },
+      {
+        id: "5",
+        name: "Meta Humor",
+        search: "meta_humor",
+      },
+    ],
+  },
+};
